17fd098e989cf2cf61cb5d9a4816390d2b234c6a
Bert Outtier <outtierbert@gmail.com>
2013-03-12 15:42:14 +0100

    added new activities

--- src/be/bertouttier/expenseapp/MenuActivity.java
+++ src/be/bertouttier/expenseapp/MenuActivity.java
@@ -1,25 +1,22 @@
 package be.bertouttier.expenseapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
 
 public class MenuActivity extends Activity {
-
-	public MenuActivity() {
-		// TODO Auto-generated constructor stub
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.split_view);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
+
 }
