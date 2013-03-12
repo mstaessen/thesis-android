819ff1e7c4f5557342003255d9cdaaf527305b26
Bert Outtier <outtierbert@gmail.com>
2013-03-12 15:58:14 +0100

    added menufragment

---
+++ src/be/bertouttier/expenseapp/MenuFragment.java
@@ -0,0 +1,34 @@
+package be.bertouttier.expenseapp;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class MenuFragment extends ListFragment {
+
+	public MenuFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.ListFragment#onListItemClick(android.widget.ListView, android.view.View, int, long)
+	 */
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.ListFragment#onViewCreated(android.view.View, android.os.Bundle)
+	 */
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onViewCreated(view, savedInstanceState);
+	}
+	
+	
+
+}
